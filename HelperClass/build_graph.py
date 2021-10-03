18781d4bd7148b86a53734acab6ed4acc474b09b
VK <vk937937@gmail.com>
2021-10-03 18:08:19 +0300

    Fix bugs visualization and add SubClassOf operation

--- HelperClass/build_graph.py
+++ HelperClass/build_graph.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 from graphviz import Source
 
@@ -54,21 +55,19 @@ class BuildGraph:
         return intersection_graph
 
     @staticmethod
-    def inverse_color(background):
-        if background.lower() == 'white':
+    def inverse_color(background1):
+        if background1.lower() == 'white':
             return 'lightgrey'
         else:
             return 'white'
-        return background
+        # return background
 
     @staticmethod
     def get_node_graph(node: Node, border_color='black', background='white', flag_negation=False):
         background_neg = background
-        if node.flag_negation:
-            background_neg = BuildGraph.inverse_color(background)
 
         if flag_negation:
-            background_neg = BuildGraph.inverse_color(background)
+            background_neg = BuildGraph.inverse_color(background_neg)
         graph = '\n\t' + node.node + f'[label=\"{node.name}\" style=filled, fillcolor={background_neg}, color={border_color}];'
         return graph
 
@@ -156,8 +155,43 @@ class BuildGraph:
     def build_name_class(class_name, line_equivalent_class):
         if len(line_equivalent_class) == 0:
             return class_name
-        class_name += '(' + ' ,'.join(line_equivalent_class) + ')'
+        class_name += ' (' + ', '.join(line_equivalent_class) + ')'
         return class_name
+
+    @staticmethod
+    def get_name_nested_node(graph: str):
+        node_name = re.search('node_[0-9]+', graph)
+        if node_name is not None:
+            return node_name.group()
+        return None
+
+    @staticmethod
+    def get_graph_subclass_of(class_graph, class_name, sub_class_of_list):
+        class_initial_graph = ''
+
+        nested_node = BuildGraph.get_name_nested_node(class_graph)
+        for node_sub_class_of in sub_class_of_list:
+            node_sub_class_of = Node(name=node_sub_class_of)
+            graph_node_sub_class_of = BuildGraph.get_node_graph(node_sub_class_of)
+            class_initial_graph += '\n\t' + graph_node_sub_class_of
+            class_initial_graph += '\n\t' + nested_node + '->' + node_sub_class_of.node + f' [ltail={class_name} lhead={nested_node} label="SubClassOf"]'
+
+        return class_initial_graph
+
+    @staticmethod
+    def get_graph_equivalent_class(equivalent_class_list):
+        class_graph = ''
+        class_initial_graph = ''
+        line_equivalent_class = []
+
+        for equivalent_class in equivalent_class_list:
+            if len(equivalent_class.elements) == 0:
+                line_equivalent_class.append(equivalent_class.name)
+                continue
+            inner_elm, graph = BuildGraph._handle_equivalent(equivalent_class.elements)
+            class_graph += graph
+            class_initial_graph += inner_elm
+        return class_initial_graph, class_graph, line_equivalent_class
 
     @staticmethod
     def _handle_onto_class(class_onto):
@@ -170,18 +204,17 @@ class BuildGraph:
             graph = BuildGraph.get_node_graph(Node(name=''), border_color='white')
             class_graph += graph
         else:
-            for equivalent_class in class_onto.equivalent_class:
-                if len(equivalent_class.elements) == 0:
-                    line_equivalent_class.append(equivalent_class.name)
-                    continue
-                inner_elm, graph = BuildGraph._handle_equivalent(equivalent_class.elements)
-                class_graph += graph
-                class_initial_graph += inner_elm
+            inner_elm, graph, line_equivalent_class = BuildGraph.get_graph_equivalent_class(class_onto.equivalent_class)
+            class_graph += graph
+            class_initial_graph += inner_elm
 
-        class_graph = BuildGraph.get_nested_graph(class_graph, 'cluster_' + class_onto.name,
+        class_name = 'cluster_' + class_onto.name
+        class_graph = BuildGraph.get_nested_graph(class_graph, class_name,
                                                   label=BuildGraph.build_name_class(class_onto.name,
                                                                                     line_equivalent_class))
-        # for equivalent_class in class_onto.equivalent_class:
+
+        if class_onto.sub_class_of:
+            class_initial_graph += BuildGraph.get_graph_subclass_of(class_graph, class_name, class_onto.sub_class_of)
 
         return class_initial_graph, class_graph
 
@@ -189,7 +222,7 @@ class BuildGraph:
         graph_main = ''
         graph_main_initial = ''
 
-        # inner_elm, graph = BuildGraph._handle_onto_class(self.data[2])
+        # inner_elm, graph = BuildGraph._handle_onto_class(self.data[1])
         # graph_main += graph
         # graph_main_initial += inner_elm
 
@@ -198,7 +231,6 @@ class BuildGraph:
             graph_main += graph
             graph_main_initial += inner_elm
         self.graph = BuildGraph._wrap_graph_in_diagram(graph_main_initial, graph_main)
-        # print(self.graph)
         self._save_to_dot_file()
         s = Source.from_file(self.path)
         s.view()
